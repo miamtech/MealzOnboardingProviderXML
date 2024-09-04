3d50a2dfc66f847c6ad6e97d7dd5c69e22af4312
diarmuidmcgonagle <diarmuidmcgonagle@gmail.com>
2024-09-04 16:19:49 +0200

    2. Initialization

--- app/src/main/java/ai/mealz/mealzonboardingproviderxml/MainActivity.kt
+++ app/src/main/java/ai/mealz/mealzonboardingproviderxml/MainActivity.kt
@@ -9,11 +9,25 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import ai.mealz.mealzonboardingproviderxml.databinding.ActivityMainBinding
 
+/**
+ * TODO (Step 2): Initialization
+ * A. Create new file MealzManager
+ * B. Add supplier key
+ * C. Init the SDK
+ * https://miamtech.github.io/mealz-documentation/docs/android/overview/supplierInit
+ */
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /**
+         * TODO (Step 2): Initialization
+         * D. Call the init method of MealzManager
+         * https://miamtech.github.io/mealz-documentation/docs/android/overview/supplierInit
+         */
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
